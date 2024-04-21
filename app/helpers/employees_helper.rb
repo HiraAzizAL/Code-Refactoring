88d955f2815de4b199b38385ab886f5e44e279cf
Hira Aziz <hira.aziz@amroodlabs.com>
2024-04-21 19:53:19 +0500

    refactor employees index links

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,2 +1,19 @@
 module EmployeesHelper
+  def paginate_links(meta)
+    return unless meta
+
+    first_page = employees_path(page: 1)
+    next_page = employees_path(page: meta["next_page"])
+    prev_page = employees_path(page: meta["prev_page"])
+    last_page = employees_path(page: meta["total_pages"])
+
+    links = [
+      link_to('First Page', first_page),
+      link_to('Next Page', next_page),
+      link_to('Previous Page', prev_page),
+      link_to('Last Page', last_page)
+    ].join(' | ').html_safe
+
+    content_tag(:div, links, class: "pagination-links")
+  end
 end
